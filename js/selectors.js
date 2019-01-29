fd16bbfa1dd45b2799d3743c8ecb347cbdc94564
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-29 12:26:39 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+    $("p");
+}
+
+function lastImageSelector() {
+    $("img:last");
+}
+
+function ninjaBabySelector() {
+    $("#baby-nina")
+}
+
+function divSelector() {
+    $(".pics")
+}
+
+function firstListItem() {
+    $("li:first-child")
+}
